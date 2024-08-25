35e2482cd2fa978d936429d0d459f3cec6c35419
hasanrahat <rahat.ruet@gmail.com>
2024-08-25 20:58:43 +0600

    final submit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+
+ <div id="app">
+    <ChatBot />
+ </div>
+ <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+</template>
+
+<script>
+import ChatBot from './components/ChatBot.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ChatBot,
+  },
+ 
+};
+</script>
+
+<style>
+#app {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  /* background-color: #eaeaea; */
+  padding: 20px; /* Adding some padding to ensure the chatbox is not too close to the edges */
+  box-sizing: border-box;
+}
+
+</style>
