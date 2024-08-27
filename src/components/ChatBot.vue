1dc9052f69e209052ab9b0b0994f2510b6f372b0
hasanrahat <rahat.ruet@gmail.com>
2024-08-28 00:11:44 +0600

    new-validation

--- src/components/ChatBot.vue
+++ src/components/ChatBot.vue
@@ -28,7 +28,6 @@ const messages = ref([]);
 let userName = ref('Mahamud Hasan');
 let messagesContainer=ref(null)
 const newMessage = ref('');
-const upComingMessage=ref('');
 
 const sendMessage = (event) => {
   if (newMessage.value.trim()) {
